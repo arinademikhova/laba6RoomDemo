eb25c5bc73b10605fef0f7f8f7454218263ef840
arina <arinademikhova@gmail.com>
2025-11-10 10:16:05 +0300

    editMainActivity1

--- app/src/main/java/com/example/roomdemo/MainActivity.kt
+++ app/src/main/java/com/example/roomdemo/MainActivity.kt
@@ -1,18 +1,32 @@
 package com.example.roomdemo
 
+import android.app.Application
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material3.*
+import androidx.compose.runtime.*
+import androidx.compose.runtime.livedata.observeAsState
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Alignment.Companion.CenterHorizontally
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.example.roomdemo.ui.theme.RoomDemoTheme
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.viewmodel.compose.viewModel
 
+import com.example.roomdemo.ui.theme.RoomDemoTheme
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -35,6 +49,54 @@ fun ScreenSetup(modifier: Modifier = Modifier) {
 fun MainScreen(modifier: Modifier = Modifier) {
 }
 
+@Composable
+fun TitleRow(head1: String, head2: String, head3: String) {
+    Row(
+        modifier = Modifier
+            .background(MaterialTheme.colorScheme.primary)
+            .fillMaxWidth()
+            .padding(6.dp)
+    ) {
+        Text(head1, color = MaterialTheme.colorScheme.onPrimary, modifier = Modifier.weight(0.1f))
+        Text(head2, color = MaterialTheme.colorScheme.onPrimary, modifier = Modifier.weight(0.6f))
+        Text(head3, color = MaterialTheme.colorScheme.onPrimary, modifier = Modifier.weight(0.3f))
+    }
+}
+
+@Composable
+fun ProductRow(id: Int, name: String, quantity: Int) {
+    Row(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(6.dp)
+    ) {
+        Text(id.toString(), modifier = Modifier.weight(0.1f))
+        Text(name, modifier = Modifier.weight(0.6f))
+        Text(quantity.toString(), modifier = Modifier.weight(0.3f))
+    }
+}
+
+@Composable
+fun CustomTextField(
+    title: String,
+    textState: String,
+    onTextChange: (String) -> Unit,
+    keyboardType: KeyboardType
+) {
+    OutlinedTextField(
+        value = textState,
+        onValueChange = { onTextChange(it) },
+        keyboardOptions = KeyboardOptions(
+            keyboardType = keyboardType
+        ),
+        singleLine = true,
+        label = { Text(title) },
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(10.dp),
+        textStyle = TextStyle(fontWeight = FontWeight.Bold, fontSize = 18.sp)
+    )
+}
 //@Preview(showBackground = true)
 //@Composable
 //fun GreetingPreview() {
